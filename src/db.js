9065ee71dfc40286cbab3958cf1e28431e730124
Shivani Peddakapu <shivanipeddakapu2@gmail.com>
2024-10-06 20:42:57 +0530

    birthday card

---
+++ src/db.js
@@ -0,0 +1,34 @@
+let db=[
+        {
+            id:1,
+            name:"Shivani",
+            age:21,
+            image:"https://cdn.pixabay.com/photo/2017/03/30/18/17/girl-2189247_1280.jpg"
+        },
+        {
+            id:2,
+            name:"Pranathi",
+            age:15,
+            image:"https://cdn.pixabay.com/photo/2024/02/04/18/27/woman-8552807_1280.jpg"
+        },
+        {
+            id:3,
+            name:"Pragathi",
+            age:17,
+            image:"https://cdn.pixabay.com/photo/2021/10/13/11/29/girl-6706267_1280.jpg "
+        },
+        {
+            id:4,
+            name:"Shivakumar",
+            age:19,
+            image:"https://cdn.pixabay.com/photo/2023/02/19/08/39/man-7799486_1280.jpg "
+        },
+        {
+            id:5,
+            name:"Chitty",
+            age:14,
+            image:"https://cdn.pixabay.com/photo/2022/06/07/15/56/child-7248693_1280.jpg "
+        }
+    ]
+
+export default db
